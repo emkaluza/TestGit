508d891fdfe869427cd974d3a67d7a95a51365dd
mkaluza <em.kaluza@gmail.com>
2018-05-01 15:23:20 +0200

    Wypisanie i

--- ModulTestowy/src/Main.java
+++ ModulTestowy/src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        Integer i = Integer.valueOf(2);
+        System.out.println("Wartość i:"+i);
     }
 }
