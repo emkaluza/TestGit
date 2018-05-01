30addf12e3f91c5cab972b00cbdd2cfe1d93405d
mkaluza <em.kaluza@gmail.com>
2018-05-01 22:15:50 +0200

    Revert "Commit2 - laczony z Commit1"
    This reverts commit cf4ce96

--- ModulTestowy/src/Main.java
+++ ModulTestowy/src/Main.java
@@ -5,8 +5,7 @@ public class Main {
         Integer i = Integer.valueOf(2);
         System.out.println("Wartość i:"+i);
 
-        System.out.println("Commit#1");
-        System.out.println("Commit#2");
-        System.out.println("Commit3");
+        System.out.println("Commit1");
+        System.out.println("Commit2");
     }
 }
